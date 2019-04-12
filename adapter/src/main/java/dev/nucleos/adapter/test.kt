db0075b6562b1063a99e9ac285a3190ce3a8b94c
Mariangela Salcedo <mariangelasalcedo@ballastlane.com>
2019-04-12 16:58:17 -0500

    commit -m "first commit"

---
+++ adapter/src/main/java/dev/nucleos/adapter/test.kt
@@ -0,0 +1,6 @@
+package dev.nucleos.adapter
+
+/**
+ * Created by Mariangela Salcedo (msalcedo047@gmail.com) on 4/12/19.
+ * Copyright (c) 2019 Nucleos dev. All rights reserved.
+ */
---
+++ app/src/main/java/dev/nucleos/genericadapter/MainActivity.kt
@@ -0,0 +1,39 @@
+package dev.nucleos.genericadapter
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Created by Mariangela Salcedo (msalcedo047@gmail.com) on 4/12/19.
+ * Copyright (c) 2019 Nucleos dev. All rights reserved.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
